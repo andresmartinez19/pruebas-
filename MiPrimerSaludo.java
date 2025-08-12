a0a340b1450558928221109c32a497974071ba68
andresmartinez19 <andres.martinezaal@cun.edu.co>
2025-08-12 12:45:51 -0500

    Add files via upload

---
+++ MiPrimerSaludo.java
@@ -0,0 +1,10 @@
+// Este programa imprime un saludo personalizado
+public class MiPrimerSaludo {
+    public static void main(String[] args) {
+        String cosa1 = "Hola";
+        String cosa2 = "adios"; 
+
+        String resultado = cosa1 + " " + cosa2;
+        System.out.println(resultado);
+    }
+}
